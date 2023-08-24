0bc7785a0b2ed6ccddb11b6c0b52cc3ea6b2ca7f
Ronak Vadodariya <ronakvadodariya1909@gmail.com>
2023-08-24 12:40:20 +0530

    changes

--- Server/index.js
+++ Server/index.js
@@ -23,13 +23,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://social-media-handle-8sii.vercel.app/"],
+    methods: ["POST", "GET", "PATCH"],
+    credentials: true,
+  })
+);
+app.use(express.json());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 // app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
