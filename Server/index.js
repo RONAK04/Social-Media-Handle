df5d78dc09c399216f577106275a7646e3862687
Ronak Vadodariya <ronakvadodariya1909@gmail.com>
2023-08-24 12:12:24 +0530

    edited

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,7 @@ import helmet from "helmet";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import {register} from "./controllers/auth.js";
+import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
@@ -34,31 +34,37 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 // app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public/assets");
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-const upload = multer({ storage:storage });
+const upload = multer({ storage: storage });
 
 // app.post("auth/register", upload.any(), register);
-app.post("/posts", verifyToken, upload.any(), createPost)
+app.post("/posts", verifyToken, upload.any(), createPost);
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+
 const PORT = process.env.PORT || 5000;
-mongoose.connect(
-    process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => {
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-    
+
     // User.insertMany(users);
     // Post.insertMany(posts);
-    }).catch((error) => console.log(`${error} did not connect`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
